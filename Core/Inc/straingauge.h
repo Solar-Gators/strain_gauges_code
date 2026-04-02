288217b3a4dd28d4d0b8b76f32c81c07d5e555b2
Joseph-Sinder <jsinder@ufl.edu>
2026-04-02 10:26:39 -0400

    created .h / .c for strain gauge, struct for data storage, pin defines in main.h

--- Core/Inc/straingauge.h
+++ Core/Inc/straingauge.h
@@ -8,3 +8,16 @@
 #pragma once
 
 #include "stm32l4xx_hal_conf.h"
+
+typedef struct {
+	I2C_HandleTypeDef* hi2c;
+	uint8_t i2c_addr;
+	//raw data from ADC, IMU
+	int16_t raw_strain;
+	int16_t strain_voltage;
+	int16_t strain_converted;
+	int16_t raw_IMU_data;
+	int8_t IMU_accel_converted;
+	int8_t IMU_gyro_converted;
+	int16_t adjusted_strain;
+} Strain_Gauge_Handle;
